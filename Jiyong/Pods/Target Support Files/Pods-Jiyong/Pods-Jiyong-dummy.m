69abd21f73e471146642b2a3a0944754a91a18f8
Simon-Meng <lexue@lexuedeMacBook-Pro.local>
2016-07-19 12:00:03 +0800

    add cocoaPod and animated-tab-bar

---
+++ Jiyong/Pods/Target Support Files/Pods-Jiyong/Pods-Jiyong-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jiyong : NSObject
+@end
+@implementation PodsDummy_Pods_Jiyong
+@end
---
+++ Jiyong/Pods/Target Support Files/Pods-JiyongUITests/Pods-JiyongUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JiyongUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JiyongUITests
+@end
